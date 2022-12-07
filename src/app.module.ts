177f6eda91002297145766a39a68af1bd514e035
gejiayang <67318944+gejiayang@users.noreply.github.com>
2022-12-07 16:55:58 +0800

    feat: 环境变量校验

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_TYPE: Joi.string().valid('mysql'),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().default(3306),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.number().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
